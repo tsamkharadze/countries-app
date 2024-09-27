0d12db32a02203a0117417395d8b00d12dc03972
tsamkharadze <samkaradze@gmail.com>
2024-09-27 19:22:15 +0400

    implement absolute paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import Header from "./components/header/Header";
-import Hero from "./components/hero/Hero";
-import CountryCard from "./components/card/Card";
+import Header from "@/components/header/Header";
+import Hero from "@/components/hero/Hero";
+import CountryCard from "@/components/card/Card";
 
 function App() {
   return (
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => {
+  const rootPath = path.resolve(process.cwd());
+  const srcPath = `${rootPath}/src`;
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "~": rootPath,
+        "@": srcPath,
+      },
+    },
+  };
+});
