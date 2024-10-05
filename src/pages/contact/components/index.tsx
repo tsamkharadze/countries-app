af3c9861d21ab6c24175347790bb444b4f3ab620
tsamkharadze <samkaradze@gmail.com>
2024-10-05 21:41:32 +0400

    task 6 V1.0 (dynammic routing and insert form element)

--- src/pages/contact/components/index.tsx
+++ src/pages/contact/components/index.tsx
@@ -1,34 +1,35 @@
 import styles from "./contact.module.css";
-const Contact = () => {
-  // const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-  // if (event.key === "Enter") {
-  // console.log("Enter pressed");
-  // }
-  // };
 
+const Contact = () => {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    // const feedback = {
-    //   name: "",
-    //   surname: "",
-    //   email: "",
-    //   message: "",
-    // };
+    const firstName = document.getElementById("firstname") as HTMLInputElement;
+    const lastName = document.getElementById("surname") as HTMLInputElement;
+    const email = document.getElementById("email") as HTMLInputElement;
+    const message = document.getElementById("message") as HTMLTextAreaElement;
 
-    console.log("raghaca");
+    const userFeedback = {
+      firstName: firstName.value,
+      lastName: lastName.value,
+      email: email.value,
+      message: message.value,
+    };
+
+    console.log("User Feedback:", userFeedback);
   };
+
   return (
     <>
       <form className={styles.form} onSubmit={handleSubmit}>
         <label>სახელი</label>
-        <input type="text" name="firstName" />
+        <input id="firstname" type="text" name="firstName" />
         <label>გვარი</label>
-        <input type="text" name="lastName" />
+        <input id="surname" />
         <label>Email</label>
-        <input type="email" name="email" />
+        <input id="email" />
         <label>შეტყობინება</label>
-        <textarea name="message"></textarea>
+        <textarea id="message"></textarea>
         <button type="submit"> SUBMIT</button>
       </form>
     </>
